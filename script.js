0ab93af870aa7739179a1aa53e4026290cadf43a
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2014-11-05 19:33:09 +0000

    Getting data from the public stream

---
+++ script.js
@@ -0,0 +1,15 @@
+$.ajax({
+   url: 'http://data.sparkfun.com/output/xROLbJzAlMcjwlN5dolp.json',
+   jsonp: 'callback',
+   cache: true,
+   dataType: 'jsonp',
+   data: {
+     page: 1
+   },
+   success: function(response) {
+     var temp = response[0]['temp'];
+     var humidity = response[0]['humidity'];
+     var timestamp = response[0]['timestamp'];
+     console.log(temp, humidity, timestamp);
+   }
+ });
