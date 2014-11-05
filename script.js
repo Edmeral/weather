1d922894c6f7237750101ea2693423378c3cb2e9
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2014-11-05 20:06:23 +0000

    checking for change every 10 seconds and updating the page

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-$.ajax({
-   url: 'http://data.sparkfun.com/output/xROLbJzAlMcjwlN5dolp.json',
-   jsonp: 'callback',
-   cache: true,
-   dataType: 'jsonp',
-   data: {
-     page: 1
-   },
-   success: function(response) {
-     var temp = response[0]['temp'];
-     var humidity = response[0]['humidity'];
-     var timestamp = response[0]['timestamp'];
+var temp, lastTemp, humidity, LastHumidity, timestamp;
 
-     $('#temperature').text('TEMPERATURE: ' + temp + ' °C');
-     $('#humidity').text('HUMIDITY: ' + humidity + ' %');
-   }
- });
+setInterval(function() {
+  $.ajax({
+     url: 'http://data.sparkfun.com/output/xROLbJzAlMcjwlN5dolp.json',
+     jsonp: 'callback',
+     cache: true,
+     dataType: 'jsonp',
+     data: {
+       page: 1
+     },
+     success: function(response) {
+
+       temp = response[0]['temp'];
+       humidity = response[0]['humidity'];
+       timestamp = response[0]['timestamp'];
+
+       // When something changes update the page
+       if (temp != lastTemp || humidity != LastHumidity) {
+         $('#temperature').text('TEMPERATURE: ' + temp + ' °C');
+         $('#humidity').text('HUMIDITY: ' + humidity + ' %');
+
+         lastTemp = temp;
+         lastHumidity = humidity;
+       }
+
+     }
+   });
+}, 10000);
