4a9f3f602feb673b7bc24600e5f9e22dc1fa901a
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2014-11-24 20:45:02 +0000

    Using XMLHttpRequest instead of JQuery's Ajax

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 var temp, lastTemp, humidity, LastHumidity, timestamp;
+var request = new XMLHttpRequest();
+var url = 'https://data.sparkfun.com/output/xROLbJzAlMcjwlN5dolp.json?page=1';
 
 function update() {
   // Getting the data from SparkFun API
-  $.ajax({
-     url: 'https://data.sparkfun.com/output/xROLbJzAlMcjwlN5dolp.json',
-     jsonp: 'callback',
-     cache: true,
-     dataType: 'jsonp',
-     data: {
-       page: 1
-     },
-     success: function(response) {
+  request.open('GET', url, true);
 
-       temp = response[0].temp;
-       humidity = response[0].humidity;
-       timestamp = response[0].timestamp;
+  request.onload = function() {
 
-       // When something changes update the page
-       if (temp != lastTemp || humidity != LastHumidity) {
-         $('#temperature').text('TEMPERATURE: ' + temp + ' °C');
-         $('#humidity').text('HUMIDITY: ' + humidity + ' %');
-         $('footer span').first().text(moment(timestamp).fromNow());
+    if (request.status >= 200 && request.status < 400) {
+      response = JSON.parse(request.responseText);
 
-         lastTemp = temp;
-         lastHumidity = humidity;
-       }
+      temp = response[0].temp;
+      humidity = response[0].humidity;
+      timestamp = response[0].timestamp;
 
-     }
-   });
+      // When something changes update the page
+      if (temp != lastTemp || humidity != LastHumidity) {
+        $('#temperature').text('TEMPERATURE: ' + temp + ' °C');
+        $('#humidity').text('HUMIDITY: ' + humidity + ' %');
+        $('footer span').first().text(moment(timestamp).fromNow());
+
+        lastTemp = temp;
+        lastHumidity = humidity;
+      }
+    }
+  };
+
+  request.onerror = function() {
+    throw new Error("Can't get the Data for Sparkfun");
+  };
+
+  request.send();
 }
 
 update();
