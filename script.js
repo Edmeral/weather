db3b4a75bcfde6574c8ce07f75202c8b7014e1f1
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2015-03-26 19:11:07 +0000

    Not using lastTemp and lastHumidity

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-var temp, lastTemp, humidity, LastHumidity, timestamp;
-
-var request = new XMLHttpRequest();
-var url = 'https://data.sparkfun.com/output/xROLbJzAlMcjwlN5dolp.json?page=1';
-
 var tempTag = document.querySelector('#temperature');
 var humidityTag = document.querySelector('#humidity');
 var timestampTag = document.querySelector('footer span');
+
+var url = 'https://data.sparkfun.com/output/xROLbJzAlMcjwlN5dolp.json?page=1';
+
+var request = new XMLHttpRequest();
+
+request.onload = function() {
+
+    if (request.status >= 200 && request.status < 400) {
+      response = JSON.parse(request.responseText);
+
+      var temp = response[0].temp;
+      var humidity = response[0].humidity;
+      var timestamp = response[0].timestamp;
+
+      tempTag.innerHTML = 'TEMPERATURE: ' + temp + ' °C';
+      humidityTag.innerHTML = 'HUMIDITY: ' + humidity + ' %';
+      timestampTag.innerHTML = moment(timestamp).fromNow();
+
+      favicon.badge(temp);
+    }
+  };
+
+  request.onerror = function() {
+    throw new Error("Can't get the data for Sparkfun");
+  };
+
 
 //Favicon configuration
 var favicon = new Favico({
@@ -19,34 +40,6 @@ var favicon = new Favico({
 function update() {
   // Getting the data from SparkFun API
   request.open('GET', url, true);
-
-  request.onload = function() {
-
-    if (request.status >= 200 && request.status < 400) {
-      response = JSON.parse(request.responseText);
-
-      temp = response[0].temp;
-      humidity = response[0].humidity;
-      timestamp = response[0].timestamp;
-
-      // When something changes update the page
-      if (temp != lastTemp || humidity != LastHumidity) {
-        tempTag.innerHTML = 'TEMPERATURE: ' + temp + ' °C';
-        humidityTag.innerHTML = 'HUMIDITY: ' + humidity + ' %';
-        timestampTag.innerHTML = moment(timestamp).fromNow();
-
-        lastTemp = temp;
-        lastHumidity = humidity;
-
-        favicon.badge(temp);
-      }
-    }
-  };
-
-  request.onerror = function() {
-    throw new Error("Can't get the Data for Sparkfun");
-  };
-
   request.send();
 }
 
